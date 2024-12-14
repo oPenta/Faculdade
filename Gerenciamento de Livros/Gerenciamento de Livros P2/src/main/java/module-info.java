db4b02736d6e421c4c992e602c24d128345eafaa
Penta <142025868+oPenta@users.noreply.github.com>
2024-12-14 09:18:43 -0300

    Delete Gerenciamento de Livros directory

--- Gerenciamento de Livros/Gerenciamento de Livros P2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.crudp2.gerenciamentodelivrosp2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.crudp2.gerenciamentodelivrosp2 to javafx.fxml;
-    exports com.crudp2.gerenciamentodelivrosp2;
-}
