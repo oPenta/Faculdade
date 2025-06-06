890b0b1b201959f165c7238a32a4b778c8da0215
Penta <142025868+oPenta@users.noreply.github.com>
2025-06-06 00:57:06 -0300

    Delete JFX/IMC JFX directory

--- JFX/IMC JFX/src/main/java/org/imc/calculoimc/utils/IDGenerator.java
+++
@@ -1,9 +0,0 @@
-package org.imc.calculoimc.utils;
-
-public class IDGenerator {
-    private static long counter = 1;
-    public static long generateID(){
-        return counter++;
-    }
-
-}
--- JFX/IMC JFX/src/main/java/org/imc/calculoimc/utils/PathFXML.java
+++
@@ -1,9 +0,0 @@
-package org.imc.calculoimc.utils;
-
-import java.nio.file.Paths;
-
-public class PathFXML {
-    public static String pathBase() {
-        return Paths.get("src/main/java/org/imc/calculoimc/view").toAbsolutePath().toString();
-    }
-}
